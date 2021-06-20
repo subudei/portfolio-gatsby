f8db5468c9bbc9b69394f26f9b4a4c11216bd6fb
Milan Cirkovic <67096753+subudei@users.noreply.github.com>
2021-06-20 22:50:43 +0200

    moveing scroll icon & contact icon to index page

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,72 +1,15 @@
-import React, { useEffect } from "react"
+import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
-// import Header from "./header/header"
 import "./layout.styles.css"
 import Nav from "./navbar/nav"
 
-import { gsap } from "gsap"
-import { ScrollTrigger } from "gsap/ScrollTrigger"
-import { BiEnvelope } from "@react-icons/all-files/bi/BiEnvelope"
-import { FaAngleDoubleDown } from "@react-icons/all-files/fa/FaAngleDoubleDown"
-import { CgMouse } from "@react-icons/all-files/cg/CgMouse"
-
 const Layout = ({ children }) => {
-  gsap.registerPlugin(ScrollTrigger)
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
-
-  useEffect(() => {
-    gsap.to(".scroll__arrows", {
-      duration: 0.7,
-      ease: "power2.out",
-      y: "2vh",
-      repeat: -1,
-      yoyo: true,
-    })
-    gsap.to(".contact__icon", {
-      scrollTrigger: {
-        trigger: ".contact__icon",
-        start: "bottom top",
-        toggleActions: "restart none none reset",
-        end: "top 90%",
-        // markers: true,
-      },
-      top: "90vh",
-      opacity: 1,
-      duration: 0.5,
-      ease: "power2.out",
-    })
-  }, [])
-
   return (
     <div className="layout__container">
       <Nav />
-      <div className="contact__icon">
-        <a
-          className="contact__mail"
-          href="mailto:milan_cirkovic@yahoo.com"
-          target="_blank"
-        >
-          <BiEnvelope className="mail__icon" />
-        </a>
-      </div>
-      <div className="scroll__icon">
-        <CgMouse className="scroll__mouse" />
-        <FaAngleDoubleDown className="scroll__arrows" />
-      </div>
-
       <main>{children}</main>
     </div>
-    // <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, { useEffect } from "react"
 import "./index.styles.css"
 
 import Layout from "../components/layout"
@@ -8,20 +8,65 @@ import About from "./about"
 import Projects from "./projects"
 import Contact from "./contact"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <div className="app__container">
-      <div className="home__container">
-        <div className="home__animation">
-          <HeroAnimation />
-        </div>
-      </div>
-      <Projects />
-      <About />
-      <Contact />
-    </div>
-  </Layout>
-)
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 
+import { FaAngleDoubleDown } from "@react-icons/all-files/fa/FaAngleDoubleDown"
+import { CgMouse } from "@react-icons/all-files/cg/CgMouse"
+import { BiEnvelope } from "@react-icons/all-files/bi/BiEnvelope"
+
+const IndexPage = () => {
+  gsap.registerPlugin(ScrollTrigger)
+
+  useEffect(() => {
+    gsap.to(".scroll__arrows", {
+      duration: 0.7,
+      ease: "power2.out",
+      y: "2vh",
+      repeat: -1,
+      yoyo: true,
+    })
+    gsap.to(".contact__icon", {
+      scrollTrigger: {
+        trigger: ".contact__icon",
+        start: "bottom top",
+        toggleActions: "restart none none reset",
+        end: "top 90%",
+        markers: true,
+      },
+      top: "90vh",
+      opacity: 1,
+      duration: 0.5,
+      ease: "power2.out",
+    })
+  }, [])
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <div className="app__container">
+        <div className="scroll__icon">
+          <CgMouse className="scroll__mouse" />
+          <FaAngleDoubleDown className="scroll__arrows" />
+        </div>
+        <div className="contact__icon">
+          <a
+            className="contact__mail"
+            href="mailto:milan_cirkovic@yahoo.com"
+            target="_blank"
+          >
+            <BiEnvelope className="mail__icon" />
+          </a>
+        </div>
+        <div className="home__container">
+          <div className="home__animation">
+            <HeroAnimation />
+          </div>
+        </div>
+        <Projects />
+        <About />
+        <Contact />
+      </div>
+    </Layout>
+  )
+}
 export default IndexPage
